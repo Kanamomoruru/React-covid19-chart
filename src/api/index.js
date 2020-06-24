bc7b6d28255fa6a26b2cf1b3406a64c5345ff6dd
Kanata <thunderpooh3@gmail.com>
2020-06-24 21:16:59 +0900

    add install chart.js

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,7 @@ export const fetchData = async () => {
 export const fetchDailyData = async () => {
     try {
         const { data } = await axios.get(`${url}/daily`);
-        console.log(data )
+        console.log(data)
     } catch (error) {
         
     }
