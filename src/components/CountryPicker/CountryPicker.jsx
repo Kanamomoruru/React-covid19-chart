cf598217212d1fed91c43dc7eebd12552452e723
Kanata <thunderpooh3@gmail.com>
2020-06-25 22:01:25 +0900

    set the coutry name using map

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -3,13 +3,25 @@ import { NativeSelect, FormControl } from '@material-ui/core';
 
 import styles from './CountryPicker.module.css';
 
-import countries from '../../api';
+import { fetchCountries } from '../../api';
 
 const CountryPickcer = () => {
+
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries());
+        }
+
+        fetchAPI();
+    }, [setFetchedCountries]);
+
     return(
         <FormControl className="styles.container">
             <NativeSelect>
                 <option value="global">Global</option>
+                {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
     )
